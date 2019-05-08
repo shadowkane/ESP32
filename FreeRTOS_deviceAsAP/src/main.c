954bd866209996a018166c795a225c945fd6822f
shadow kane <shadoo.kane@live.com>
2019-05-08 18:24:51 +0100

    code for AP using FreeRTOS

--- FreeRTOS_deviceAsAP/src/main.c
+++ FreeRTOS_deviceAsAP/src/main.c
@@ -22,12 +22,54 @@
 #include <esp_event_loop.h>
 #include <esp_log.h>
 
+// this function will get 4 paramaters of type unsigned integer 32 bits, representing each octet of the IP address and return it as one value of type 32 bits
+// an IP address is divided in 4 words (octets) ex: 192.168.1.1, where the fourth octet (192) is actualy the first octet in the network byte order which is used by all TCP/IP protocols
+// it is the same thing as big endian or the MSB first. 
+u32_t makeU32_Address(u32_t firstSet, u32_t secondSet, u32_t thirdSet, u32_t fourthSet){
+    u32_t address = firstSet;
+    address += secondSet << 8;
+    address += thirdSet << 16;
+    address += fourthSet << 24;
+    return address;
+}
+
 esp_err_t eventHandler(void *ctx, system_event_t *event){
     switch (event->event_id)
     {
     case SYSTEM_EVENT_AP_START:
         // run this section when the wifi driver started as AP
         printf("System AP started.\n");
+        // set a local IP@ along with a gatway @ and subnet mask
+        // the addr variable is of type u32_t    
+        tcpip_adapter_ip_info_t defaultIPConfig = {
+            .ip.addr = makeU32_Address(192,168,1,1),
+            .gw.addr = makeU32_Address(192,168,1,1),
+            .netmask.addr = makeU32_Address(255,255,255,0)
+        };
+        // before set the TCPIP configuration, stop the DHCP server
+        ESP_ERROR_CHECK(tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP));
+        // set the tcpip configuration
+        esp_err_t ret = tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &defaultIPConfig);
+        ESP_ERROR_CHECK(ret);
+        // start the DHCP if the ip info setup was right
+        if(ret == ESP_OK){
+            // the lease time option is to give the client the option to allocate a place in the server for a periode of time.
+            // but in our context we are using lease to allocate IP addresses so the server assign them to incoming clients
+            dhcps_lease_t leaseConfig = {
+                .enable = true,
+                .start_ip.addr = makeU32_Address(192,168,1,2),
+                .end_ip.addr = makeU32_Address(192,168,1,11)
+            };
+            // tcpip_adapter_dhcps_option this function to set or get the DHCP server option
+            // using TCPIP_ADAPTER_OP_SET option to set an option to the DHCP server
+            // TCPIP_ADAPTER_REQUESTED_IP_ADDRESS to set IP address
+            ESP_ERROR_CHECK(tcpip_adapter_dhcps_option((tcpip_adapter_option_mode_t)TCPIP_ADAPTER_OP_SET, (tcpip_adapter_option_id_t)TCPIP_ADAPTER_REQUESTED_IP_ADDRESS,(void*) &leaseConfig, sizeof(dhcps_lease_t)));
+            ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
+        }
+        tcpip_adapter_ip_info_t IPInfo;
+        tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP, &IPInfo);
+        printf("AP IP from ip info@: %s\n", ip4addr_ntoa(&IPInfo.ip.addr));
+
         // get the AP IP@ 
         printf("AP IP@: %s\n", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
         // get the AP MAC@
@@ -91,5 +133,5 @@ void app_main(){
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(nvs_flash_erase());
     // start AP task
-    xTaskCreate(&AP_task, "AP task", 8000, NULL, 1, NULL);
+    xTaskCreate(&AP_task, "AP task", 10000, NULL, 1, NULL);
 }
